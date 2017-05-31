3bd2fcf1434e7ec5ca223dde7fe7e40a5dd86e03
Yongkwan Lim <ykwan0714@gmail.com>
2017-06-01 00:41:51 +0900

    170601 /dev/todo commit

--- dev/todo/src/App.js
+++ dev/todo/src/App.js
@@ -23,13 +23,15 @@ class App extends React.Component {
 					id : 2,
 					contents : '오늘 할 일3',
 					isDone : false
-				},
-
+				}
 			]
 		}
 		this.toggleTodo = this.toggleTodo.bind(this);
+		this.toggleAll = this.toggleAll.bind(this);
 		this.deleteTodo = this.deleteTodo.bind(this);
+		this.addTodo = this.addTodo.bind(this);
 	}
+
 	/* todo를 toggle하는 function */
 	toggleTodo(id){
 		const newTodos = [...this.state.todos];
@@ -42,6 +44,45 @@ class App extends React.Component {
 			todos : newTodos
 		});
 	}
+	/* 전부 체크일 때 전부 해제, 그 외에엔 모두 체크 하는 functon*/
+	toggleAll(){
+
+		const activate = this.state.todos.some(elem=>!elem.isDone); // isDone이 false->true 그 반대도 되야한다.
+		const todos = [...this.state.todos];
+		this.setState({
+				todos : todos.map((elem, index)=>{
+					return Object.assign({},elem,{
+						isDone : activate
+					})
+				})
+		});
+		/*const isAllActivate = todos.every(elem=>elem.isDone); // 모두 true면 true
+		if(isAllActivate){ // 모두 해제
+			const newTodos = todos.map((elem, index)=>{
+				return Object.assign({},elem,{
+					isDone : false
+				})
+			});
+			//newTodos.isChecked = false;
+			this.setState({
+				todos : newTodos,
+				isChecked : false
+			});
+		}else{ // 모두 선택
+			const newTodos = todos.map((elem, index)=>{
+				return Object.assign({},elem,{
+					isDone : true
+				})
+			});
+			//newTodos.isChecked = true;
+			this.setState({
+				todos : newTodos,
+				isChecked : true
+			});
+		}*/
+
+
+	};
 	/* todo를 delete하는 function */
 	deleteTodo(id){
 		const newTodos = [...this.state.todos];
@@ -52,11 +93,39 @@ class App extends React.Component {
 			todos : newTodos
 		});
 	}
+	/* todo를 add하는 function */
+	addTodo(contents){
+		/*
+		const todo = {
+			id : this.state.todos.length,
+			contents : contents,
+			isDone : false
+		};
+		const newTodos = [...this.state.todos, todo];
+		*/
+		this.setState({
+			todos :
+			[
+				...this.state.todos,
+				{
+					id : this.state.todos.length,
+					contents : contents,
+					isDone : false
+				}
+			]
+		});
+
+
+	}
 	render() {
 
 		return(
 			<div className='todo-app'>
-				<Header/>
+				<Header
+					addTodo={this.addTodo}
+					toggleAll={this.toggleAll}
+					isChecked={this.state.todos.every(elem=>elem.isDone)}
+				/>
 				<TodoList
 					todos = {this.state.todos}
 					toggleTodo = {this.toggleTodo}
--- dev/todo/src/Header.js
+++ dev/todo/src/Header.js
@@ -9,7 +9,16 @@ const defaultProps = {
 class Header extends Component {
 	constructor(props) {
 		super(props);
+		this.handleKeyDown = this.handleKeyDown.bind(this);
 	}
+	handleKeyDown(event){
+		const content = event.target.value;
+		if(content.length>0 && event.keyCode == 13){
+			this.props.addTodo(content);
+			event.target.value = '';
+		}
+	}
+
 	render() {
 		return(
 			<header>
@@ -18,8 +27,14 @@ class Header extends Component {
 					type='text'
 					className='todo-app__new-todo'
 					placeholder='What needs to be done?'
+					onKeyDown={this.handleKeyDown}
 				/>
-				<button className='toggle-all'/>
+				<button
+					className={
+						ClassNames('toggle-all', {'checked' : this.props.isChecked})
+					}
+					onClick={this.props.toggleAll}
+				/>
 			</header>
 
 		);
